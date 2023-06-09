63bf62296997e5714af4616b5ed4e81f065a3ffd
Victor Vazquez <vhvb1989@gmail.com>
2023-06-09 14:36:40 -0700

    leia

---
+++ leia/src/Leia.js
@@ -0,0 +1,15 @@
+import './Leia.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is R2
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
